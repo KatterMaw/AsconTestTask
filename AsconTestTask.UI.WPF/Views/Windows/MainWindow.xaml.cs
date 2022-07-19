d993562ab64360a69549385e942c73fe2de6e93e
KatterMaw <TheKatterDev@gmail.com>
2022-07-19 15:06:01 +0500

    validate db model

--- AsconTestTask.UI.WPF/Views/Windows/MainWindow.xaml.cs
+++ AsconTestTask.UI.WPF/Views/Windows/MainWindow.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace AsconTestTask.UI.WPF.Views.Windows;
+﻿using System;
+using AsconTestTask.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsconTestTask.UI.WPF.Views.Windows;
 
 public partial class MainWindow
 {
 	public MainWindow()
 	{
+		var dbContext = new AppDbContext();
+		try
+		{
+			dbContext.Database.EnsureCreated();
+		}
+		catch
+		{
+			dbContext.Database.EnsureDeleted();
+			dbContext.Database.EnsureCreated();
+		}
+		
 		InitializeComponent();
 	}
 }
